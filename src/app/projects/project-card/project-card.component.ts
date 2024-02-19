292f254fbaab5b6a8ec09f45a2e7cb64870823eb
ernest-lamptey <1ernestlamptey@gmail.com>
2024-02-19 23:01:26 +0100

    added project detail page

--- src/app/projects/project-card/project-card.component.ts
+++ src/app/projects/project-card/project-card.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./project-card.component.scss']
 })
 export class ProjectCardComponent {
-  randomId = Math.floor(Math.random() * 500)
+  randomId = Math.floor(Math.random() * 50)
   imageUrl = `https://picsum.photos/id/${this.randomId}/600/300`;
 
   @Input() project:any = {};
