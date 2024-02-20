77c5f61e179df7aa806a4065544de01407c84742
ernest-lamptey <1ernestlamptey@gmail.com>
2024-02-20 16:56:59 +0100

    add images to db.json file

--- src/app/projects/project-detail/project-detail.component.ts
+++ src/app/projects/project-detail/project-detail.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../project.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
   styleUrls: ['./project-detail.component.scss']
 })
-export class ProjectDetailComponent {
+export class ProjectDetailComponent implements OnInit {
+  project: any
+  constructor(private projectService: ProjectService,
+    private activatedRoute: ActivatedRoute){}
 
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.projectService.getOneProject(id).subscribe(res => {
+      this.project = res;
+      // console.log(this.project)
+    })
+  }
 }
