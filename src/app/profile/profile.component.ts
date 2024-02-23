5586921a3f0867f560b12ec8e7765f972bad1946
ernest-lamptey <1ernestlamptey@gmail.com>
2024-02-23 21:18:11 +0100

    add user service

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from './profile.service';
+import { UserService } from '../shared/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,10 +8,15 @@ import { ProfileService } from './profile.service';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  constructor(private profileService: ProfileService){}
+  constructor(private profileService: ProfileService,
+    private userService: UserService){}
 
   ngOnInit() {
     this.profileService.getProfile("1").subscribe((res) => {
+      console.log(res)
+    })
+
+    this.userService.getUser("1").subscribe((res) => {
       console.log(res)
     })
   }
