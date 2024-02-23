0099d729d70753b7a479db57a9f0521385f965ad
ernest-lamptey <1ernestlamptey@gmail.com>
2024-02-23 21:57:07 +0100

    set base for profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from './profile.service';
 import { UserService } from '../shared/user.service';
+import { faChevronLeft, faEllipsis, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile',
@@ -8,16 +9,23 @@ import { UserService } from '../shared/user.service';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  faChevronLeft = faChevronLeft;
+  faEllipsis = faEllipsis;
+  user!:any;
+  profile!:any;
+
   constructor(private profileService: ProfileService,
     private userService: UserService){}
 
   ngOnInit() {
     this.profileService.getProfile("1").subscribe((res) => {
+      this.profile = res;
       console.log(res)
     })
 
     this.userService.getUser("1").subscribe((res) => {
-      console.log(res)
+      this.user = res;
+      // console.log(res)
     })
   }
 }
